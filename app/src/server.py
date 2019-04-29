37d830fbab624db37e66a47b6bc09605f4cbbefe
Samuel Oyegbemi <samuel.oyegbemi@supermartng.com>
2019-04-29 14:33:06 +0100

    first successful build

--- app/src/server.py
+++ app/src/server.py
@@ -4,15 +4,18 @@ import uuid
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     hostname = os.uname()[1]
     randomid = uuid.uuid4()
-    return jsonify({"Hostname" : hostname, "UUID": str(randomid)})
+    return jsonify({"Hostname": hostname, "UUID": str(randomid)})
+
 
 @app.route('/login')
 def login():
     return 'Hi, I am in login page, and i am not updating'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
